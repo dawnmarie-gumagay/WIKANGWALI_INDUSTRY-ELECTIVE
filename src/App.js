578f3b04a290cdcf655f189157134ae6c039a9d6
paez-marie1019 <paez.marie1019@gmail.com>
2023-11-21 17:22:14 +0800

    Logged In for App.js
    Removed LoggedOut component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import './App.css';
+// App.js
+import React, { useState } from 'react';
 import LoggedIn from './LoggedIn';
 import LoggedOut from './LoggedOut';
+import './App.css';
+import LandingPage from './LoggedOutPages/LandingPage';
+import SignInPage from './LoggedOutPages/SignInPage';
+import SignUpPage from './LoggedOutPages/SignUpPage';
+import Signing from './LoggedOutPages/Signing';
+import { Route, Routes, Link } from 'react-router-dom';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
-    <div className="logged-in">
+    <div className="App">
       <head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Love+Ya+Like+A+Sister&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" />
       </head>
-      <LoggedIn/>
-      <h1>GIT pract</h1>
-      <LoggedOut/>
+
+      {isLoggedIn ? (
+        <LoggedIn />
+      ) : (
+        <div className="loggedout-container">
+          <Routes>
+            <Route path="/" element={<LandingPage/>}/>
+            <Route path="/Signing" element={<Signing />} />
+            <Route path="/SignInPage" element={<SignInPage onLogin={handleLogin}/>}/>
+            <Route path="/SignUpPage" element={<SignUpPage/>}/>
+          </Routes>
+        </div>
+      )}
     </div>
   );
 }
